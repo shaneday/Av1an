4c85d937fffdfdbdf2fd0856efc6a9f7764bd177
Shane Day <shane.a.day@gmail.com>
2024-06-05 12:30:45 +1200

    Fix build on OSX, linker couldn't find libvapoursynth.a

--- av1an/build.rs
+++ av1an/build.rs
@@ -3,6 +3,12 @@ use std::error::Error;
 use vergen::EmitBuilder;
 
 fn main() -> Result<(), Box<dyn Error>> {
+
+  // On OSX, the linker doesn't include homebrew path (needed to find libvapoursynth.a)
+  if cfg!(target_os = "macos") {
+    println!("cargo:rustc-link-search=native=/opt/homebrew/lib");
+  }
+
   EmitBuilder::builder()
     .git_sha(true)
     .git_commit_date()
